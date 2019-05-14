f344ab7a36526d304d4a6c6e3f7d52e52e2c30ec
RosanaRufer <rosanarufer@gmail.com>
2019-05-14 20:05:01 +0100

    False: is_leap(5) => should be False

--- Leap_Year/test_is_leap.py
+++ Leap_Year/test_is_leap.py
@@ -6,3 +6,4 @@ from Leap_Year.is_leap import is_leap
 class TesIsLeap(TestCase):
     def test_is_leap(self):
         self.assertEqual(is_leap(4), True)
+        self.assertEqual(is_leap(5), False)
