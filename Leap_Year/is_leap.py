77d1202bd22447f93af96bcdc57f6c98e95874d4
RosanaRufer <rosanarufer@gmail.com>
2019-05-14 20:21:35 +0100

    Passing: is_leap(500) => should be False

--- Leap_Year/is_leap.py
+++ Leap_Year/is_leap.py
@@ -1,2 +1,8 @@
 def is_leap(year):
-    return year % 4 == 0
+    if is_divisible(year, 100) and not is_divisible(year, 400):
+        return False
+    return is_divisible(year, 4)
+
+
+def is_divisible(dividend, divisor):
+    return dividend % divisor == 0
