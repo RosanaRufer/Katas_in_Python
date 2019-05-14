4cc9edfdc18fe72e23b4dfc4573200c9408ddb22
RosanaRufer <rosanarufer@gmail.com>
2019-05-14 20:25:56 +0100

    Refactor tests

--- Leap_Year/test_is_leap.py
+++ Leap_Year/test_is_leap.py
@@ -4,7 +4,13 @@ from Leap_Year.is_leap import is_leap
 
 
 class TesIsLeap(TestCase):
+
     def test_is_leap(self):
-        self.assertEqual(is_leap(4), True)
-        self.assertEqual(is_leap(5), False)
-        self.assertEqual(is_leap(500), False)
+        should_be_leap = [4, 1996, 1600]
+        for number in should_be_leap:
+            self.assertEqual(is_leap(number), True)
+
+    def test_is_not_leap(self):
+        should_not_be_leap = [5, 500, 1997, 1800]
+        for number in should_not_be_leap:
+            self.assertEqual(is_leap(number), False)
