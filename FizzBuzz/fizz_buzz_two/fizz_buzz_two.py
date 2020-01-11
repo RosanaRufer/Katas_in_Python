f4789b09158759b34ae1cec6561ab212da64c275
RosanaRufer <rosanarufer@gmail.com>
2020-01-11 11:30:21 +0000

    Refactor

--- FizzBuzz/fizz_buzz_two/fizz_buzz_two.py
+++ FizzBuzz/fizz_buzz_two/fizz_buzz_two.py
@@ -1,5 +1,8 @@
+def divisible_by_3(number: int):
+    return number % 3 == 0
+
 
 def fizz_buzz_two(number: int):
-    if number == 3 or number == 9:
+    if divisible_by_3(number):
         return "Fizz"
     return str(number)
