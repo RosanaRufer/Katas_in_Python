f281769d007bbef1672a1f4abab31f1b2b88dc3e
RosanaRufer <rosanarufer@gmail.com>
2020-01-11 10:29:02 +0000

    Fail: 2=> "2"

--- FizzBuzz/fizz_buzz_two/test_fizz_buzz_two.py
+++ FizzBuzz/fizz_buzz_two/test_fizz_buzz_two.py
@@ -1,8 +1,15 @@
 from FizzBuzz.fizz_buzz_two.fizz_buzz_two import fizz_buzz_two
 
 
-def test_general_case():
+def test_general_case_one():
     """
     When I fizzbuzz number 1, I get back a string representing it
     """
     assert fizz_buzz_two(1) == "1"
+
+def test_general_case_two():
+
+    """
+    When I fizzbuzz number 2, I get back a string representing it
+    """
+    assert fizz_buzz_two(2) == "2"
