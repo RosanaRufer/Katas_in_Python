a64945ea490613f78bc0dda5e9f239247ff75296
RosanaRufer <rosanarufer@gmail.com>
2020-02-09 10:14:52 +0000

    Refactor

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -1,3 +1,7 @@
+STRIKE = 'X'
+SPARE = '/'
+
+
 def score_for(all_rolls):
     """
     Given a set of bowling frames
@@ -6,34 +10,34 @@ def score_for(all_rolls):
     :return: int
     """
     frames = all_rolls.split('|')
-    nframes = len(frames)
+    last_index = len(frames) - 1
+    total_score = 0
 
-    if nframes > 1:
-        index = 0
-        acc = 0
-        while index < 10 and nframes > index:
-            sff = score_for_frame(frames[index])
-            acc = acc + sff
-            if ('X' in frames[index]) and nframes > index:
-                acc = acc + score_for_frame(frames[index + 1])
-            if ('/' in frames[index]) and nframes > index:
-                acc = acc + score_for_first_roll(frames[index + 1])
-            index = index + 1
-        return acc
-    else:
-        return score_for_frame(all_rolls)
+    for index, frame in enumerate(frames):
+        current_frame_score = score_for_frame(frame)
+
+        next_index = index + 1
+
+        if (STRIKE in frame) and next_index <= last_index:
+            current_frame_score = current_frame_score + score_for_frame(frames[next_index])
+        if (SPARE in frame) and next_index <= last_index:
+            current_frame_score = current_frame_score + score_for_first_roll(frames[next_index])
+
+        total_score = total_score + current_frame_score
+
+    return total_score
 
 
 def score_for_first_roll(frame):
     first_roll = frame[0]
-    if first_roll in ['X', '/']:
+    if first_roll in [STRIKE, SPARE]:
         return 10
     else:
         return int(first_roll)
 
 
 def score_for_frame(frame):
-    if frame == 'X' or '/' in frame:
+    if SPARE in frame or STRIKE in frame:
         return 10
     else:
         numbers = [int(n) for n in list(frame)]
